d579db0f86af3bb1e237c243473b1ea46c53c8c1
tungbeo234 <sirinvahitpun0suy@hotmail.com>
2024-05-16 01:32:33 +0700

    test commit

--- index.js
+++ index.js
@@ -1,30 +1,10 @@
-function numIslands(grid) {
-  if (grid.length === 0 || grid[0].length === 0) return 0;
-  const rows = grid.length;
-  const cols = grid[0].length;
-  let islands = 0;
-  for (let i = 0; i < rows; i++) {
-    for (let j = 0; j < cols; j++) {
-      if (grid[i][j] === "1") {
-        islands++;
-        dfs(i, j);
-      }
-    }
+function findMin(nums) {
+  let left = 0;
+  let right = nums.length - 1;
+  while (left < right) {
+    const mid = Math.floor((left + right) / 2);
+    if (nums[mid] > nums[right]) left = mid + 1;
+    else right = mid;
   }
-  return islands;
-  function dfs(row, col) {
-    if (
-      row < 0 ||
-      row >= rows ||
-      col < 0 ||
-      col >= cols ||
-      grid[row][col] !== "1"
-    )
-      return;
-    grid[row][col] = "0";
-    dfs(row + 1, col);
-    dfs(row - 1, col);
-    dfs(row, col + 1);
-    dfs(row, col - 1);
-  }
+  return nums[left];
 }
